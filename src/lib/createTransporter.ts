3b4005725079e6adca1b14796ff7a36e395ea550
sudais-khan12 <155130846+sudais-khan12@users.noreply.github.com>
2025-02-16 14:17:25 +0500

    transporter fixed

--- src/lib/createTransporter.ts
+++ src/lib/createTransporter.ts
@@ -3,11 +3,17 @@ import type { Transporter } from "nodemailer";
 
 const createTransporter = (): Transporter => {
   const transporter = nodemailer.createTransport({
-    host: "gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
     auth: {
       user: process.env.USER_EMAIL,
       pass: process.env.USER_PASS,
     },
+    tls: {
+      rejectUnauthorized: false,
+    },
+    logger: true,
   });
 
   return transporter;
