d3f6a39ef5a8baef9add20a4185ae23af2412f65
sudais-khan12 <155130846+sudais-khan12@users.noreply.github.com>
2025-03-01 13:47:23 +0500

    session fixed

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -107,7 +107,20 @@ export const authOptions: NextAuthOptions = {
 
   session: {
     strategy: "jwt",
-    maxAge: 30 * 24 * 60 * 60,
+    maxAge: 30 * 24 * 60 * 60, // 30 days
+  },
+
+  cookies: {
+    sessionToken: {
+      name: `next-auth.session-token`,
+      options: {
+        httpOnly: true,
+        sameSite: "lax",
+        path: "/",
+        secure: process.env.NODE_ENV === "production", // Ensure cookies are only sent over HTTPS in production
+        maxAge: 30 * 24 * 60 * 60, // 30 days
+      },
+    },
   },
 
   pages: {
